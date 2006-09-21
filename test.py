7b15d96a80e47fca52bf010b649b34ab38fecbb0
kripkensteiner <kripkensteiner@3d94fd04-e71d-0410-b633-9d8812b22738>
2006-09-21 08:14:26 +0000

    identity change

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
+#/* 
+#Copyright: A. Zakai ('Kripken') <kripkensteiner@gmail.com> http://6thsenseless.blogspot.com
+#
+#2006-15-9
+#
+#This code is licensed under the terms of the GNU General Public License (GPL),
+#version 2 or above; See /usr/share/common-licenses/GPL , or see
+#http://www.fsf.org/licensing/licenses/gpl.html
+#*/
+
+
 import torrent
 from   time    import sleep
 
